90ef888c6816b3151a98f30bd5dc0b3b3a2f6d5c
Rahul Arepaka <rahul.arepaka@gmail.com>
2021-06-07 11:57:45 +0400

    Updated

---
+++ Code/Relay-On/Relay-On.ino
@@ -0,0 +1,14 @@
+int relaypin = 6;
+
+void setup(){
+  pinMode(relaypin,OUTPUT);
+}
+
+
+void loop(){
+  //digitalWrite(relaypin, HIGH);
+  //delay(1000);
+
+  digitalWrite(relaypin, LOW);
+  //delay(1000);
+}
